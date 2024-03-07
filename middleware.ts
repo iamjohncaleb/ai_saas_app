90daa4bec41f62ef0714587cdf3b7036d8f1588c
Caleb John <johncaleb022@gmail.com>
2024-03-07 02:44:24 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
- 
+
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
